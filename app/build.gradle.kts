5ec640ffed33e23a3ecfe0bdcab0b1b2b55363b1
Tiyas Insania Dhikananda <tiyasinsania@gmail.com>
2025-06-13 02:31:42 +0700

    Konfigurasi dependensi jaringan dan database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.compose.material)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.firebase.firestore.ktx)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
